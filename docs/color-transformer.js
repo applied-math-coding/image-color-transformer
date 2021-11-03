1bdc1748b27bee514dd8a4a20d50638c59d939eb
Your Name <you@example.com>
2021-11-03 15:12:08 +0200

    deploy to github pages

--- docs/color-transformer.js
+++ docs/color-transformer.js
@@ -1,3 +1,5 @@
+self.importScripts('./transformer-utils.js');
+
 onmessage = ({ data: { imageData, palette } }) => {
   const result = new Uint8ClampedArray(imageData);
   const rgbColors = palette.map(c => toRgb(c));
@@ -10,35 +12,3 @@ onmessage = ({ data: { imageData, palette } }) => {
   postMessage(result.buffer, [result.buffer]);
 }
 
-function distance(c1, c2) {
-  return Math.max(...c1.map((v, idx) => Math.abs(v - c2[idx])));
-}
-
-function* enumerate(array) {
-  for (let [idx, colorIdx] = [0, 0]; idx + 3 < array.length; [idx, colorIdx] = [idx + 4, colorIdx + 1]) {
-    yield {
-      color: [array[idx], array[idx + 1], array[idx + 2]],
-      colorIdx
-    };
-  }
-}
-
-function setColor(colorIdx, target, color) {
-  color.forEach((v, idx) => {
-    target[4 * colorIdx + idx] = v;
-  });
-}
-
-function toRgb(c) {
-  return c
-    .substring(1)
-    .split('')
-    .reduce((prev, cur, idx) => {
-      if (idx % 2 === 0) {
-        prev.push(cur);
-      } else {
-        prev[prev.length - 1] = parseInt(prev[prev.length - 1] + cur, 16);
-      }
-      return prev;
-    }, []);
-}
